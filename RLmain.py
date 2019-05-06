c955f021a679d73321be08bbb997cf68009f8345
Yangcy <13520183356@139.com>
2019-05-06 23:31:16 +0800

    debug no tf

--- RLmain.py
+++ RLmain.py
@@ -21,6 +21,7 @@ import utils.flops as flops
 from agent import Agent,LinearSchedule
 import numpy as np
 from utils import logger
+import tensorflow as tf
 
 def accuracy(output, target, topk=(1,)):
     """Computes the precision@k for the specified values of k"""
