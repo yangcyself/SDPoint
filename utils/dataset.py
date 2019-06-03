c78e7d9decc59fc0c7787377cf215c8dea3c31e9
Yangcy <13520183356@139.com>
2019-06-03 14:08:58 +0800

    added tinyImagenet

--- utils/dataset.py
+++ utils/dataset.py
@@ -84,6 +84,84 @@ class StanfordDog(data.Dataset):
     def __len__(self):
         return len(self.imgs)
 
+
+class TinyImageNet(data.Dataset):
+    """
+    Loading TinyImageNet dataset
+    """
+    def __init__(self, datasetRoot, transforms=None, train=True, already=False):
+        """
+        Initialization of the dataset
+        root : place holder of the tinyImagenet dataset
+        transforms : required transformation of the images
+        train / test : getting training set or testing set
+
+        """
+        if transforms is None:
+            self.transforms = T.Compose([
+                T.ToTensor()
+                ])
+        if already:
+            self.imgs = []
+            if os.path.exists(path=os.path.join(datasetRoot,"tinyImageNet.pkl")):
+                with open(os.path.join(datasetRoot,"tinyImageNet.pkl"),'rb') as load_data:
+                    self.imgs, self.labels = pickle.load(load_data)
+            for img, label in zip(self.imgs, self.labels):
+                self.breed_dict[label] = img
+        else:
+            self.train = train
+            self.imgs = []
+            self.labels = []
+            self.classes = [] # the string nxxxxxx of different classes 
+            self.ids = {} # mapping the class string nxxxx to number start from 0
+            wnids = os.path.join(datasetRoot,"wnids.txt")
+            with open(wnids,"r") as f:
+                ids = f.read()
+            self.classes = ids.split()
+            num = 0
+            for c in self.classes:
+                self.ids[c] = num
+                num += 1
+            print("TOTAL CLASSES:", num)
+
+            if self.train:
+                imgpath = os.path.join(datasetRoot,"train")
+                for c in self.classes:
+                    classpath = os.path.join(imgpath,c,"images") # another file in imgpath/c is boxes.txt
+                    for i in os.listdir(classpath):
+                        ipath = os.path.join(classpath,i)
+                        img = cv2.imread(ipath)
+                        self.imgs.append(img)
+                        self.labels.append(self.ids[c])
+            else:
+                imgpath = os.path.join(datasetRoot,"val") # the lay out in val folder is a images folder with all images in it 
+                                                          # and a val_annotations.txt
+                annota = os.path.join(imgpath,"val_annotations.txt")
+                with open(annota,"r") as f:
+                    for l in f.readlines():
+                        iname,c,_,__,___,____ = l.split()
+                        ipath = os.path.join(imgpath,"images",iname)
+                        img = cv2.imread(ipath)
+                        self.imgs.append(img)
+                        self.labels.append(self.ids[c])
+            
+
+    def save(self):
+        with open(os.path.join(datasetRoot,"tinyImageNet.pkl"), 'wb') as save_data:
+            data_list = [self.imgs, self.labels]
+            pickle.dump(data_list, save_data)
+
+    def __getitem__(self, index):
+        # tmp = cv2.resize(self.imgs[index], (96, 96))
+        tmp = cv2.cvtColor(self.imgs[index], cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(tmp)
+        img = self.transforms(img)
+        return img, self.labels[index]
+
+    def __len__(self):
+        return len(self.imgs)
+
+
 if __name__=='__main__':
     sd = StanfordDog('.', '.')
     sd.save()
